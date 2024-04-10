843b8e6386b766e686c9a6031b8343e5f289c5de
Shamanth Ganiger <shamanthganiger@gmail.com>
2024-04-10 18:51:23 +0530

    for testing free hosting on render with / route

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,12 @@ app.use(express.json());
 
 const mainRouter = require("./routes/index");
 
+app.get("/", (req,res)=>{
+    res.json({
+        message: "Server started"
+    })
+})
+
 app.use("/api/v1", mainRouter ); // /api/v1/user/signup, /api/v1/user/signin, /api/v1/user/, /api/v1/user/bulk
                                  // /api/v1/account/balance, /api/v1/transfer
 
